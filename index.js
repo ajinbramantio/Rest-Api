a6f1ae5b18cef528becba94a4e6c34fef9147d61
ajinbramantio <ajinbramantio@gmail.com>
2019-02-18 16:16:04 +0700

    Learning rest api

---
+++ index.js
@@ -0,0 +1,10 @@
+const axios = require('axios')
+axios
+  .get('https://api.github.com/users/ajinbramantio')
+  .then(response => {
+    // Response is already in JSON format
+    console.log(response.data)
+  })
+  .catch(error => {
+    console.log(error)
+  })
